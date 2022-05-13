366398fd910aa880d14d6596695e3e4ac4abde51
Philipp Stucki <ps@stuckistucki.com>
2022-05-13 14:12:42 +0200

    remove layout state

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,9 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { useState } from "react";
 
 const CustomApp = ({ Component, pageProps }: AppProps) => {
-  const [menu, setMenu] = useState(false);
-
   return (
     <div id="customApp">
-      <h1>Hello From App Component</h1>
-      <button onClick={() => setMenu(!menu)}>{JSON.stringify(menu)}</button>
       <Component {...pageProps} />
     </div>
   );
